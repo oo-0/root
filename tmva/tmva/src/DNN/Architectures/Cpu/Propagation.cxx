1f9613f5ff7b501cb0989a5ccb928b74ae93a794
Ask149 <ashishkshirsagar10@gmail.com>
2019-07-01 20:24:22 +0530

    Remove indexing error

--- tmva/tmva/src/DNN/Architectures/Cpu/Propagation.cxx
+++ tmva/tmva/src/DNN/Architectures/Cpu/Propagation.cxx
@@ -399,7 +399,7 @@ void TCpu<AFloat>::GenerateColumnarMatrix(TCpuMatrix<AFloat> input,
       std::cout<<input(i,j)<<std::endl;
       std::cout<<"Input Columnar Indices "<<(i*input.GetNcols())<<"+"<<j<<", "<<0<<std::endl;
       inputColumnar[0](i*input.GetNcols()+j,0) = input(i,j);
-      std::cout<<inputColumnar[0](i*input.GetNcols()+j,1)<<" ";
+      std::cout<<inputColumnar[0](i*input.GetNcols()+j,0)<<" ";
     }
     std::cout<<std::endl;
   }
